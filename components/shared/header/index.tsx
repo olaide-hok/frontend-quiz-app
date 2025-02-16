000cae0403598774dc27122687c3a6a48aa5c6e1
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-16 08:02:37 +0100

    header component

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,12 @@
+import ModeToggle from './mode-toggle';
+
+const Header = () => {
+    return (
+        <>
+            Header
+            <ModeToggle />
+        </>
+    );
+};
+
+export default Header;
