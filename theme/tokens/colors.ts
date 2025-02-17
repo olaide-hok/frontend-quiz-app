1e9439d56709558bd70b7f414167530639ddd4d6
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-17 21:13:37 +0100

    update color names

--- theme/tokens/colors.ts
+++ theme/tokens/colors.ts
@@ -13,12 +13,12 @@ export const colors = defineTokens.colors({
     white: {
         value: '#FFFFFF',
     },
-    purple: {value: '#A729F5'},
+    brandPurple: {value: '#A729F5'},
     darkNavy: {value: '#313E51'},
-    navy: {value: '#3B4D66'},
+    brandNavy: {value: '#3B4D66'},
     greyNavy: {value: '#626C7F'},
     lightBluish: {value: '#ABC1E1'},
     lightGrey: {value: '#F4F6FA'},
-    green: {value: '#26D782'},
-    red: {value: '#EE5454'},
+    brandGreen: {value: '#26D782'},
+    brandRed: {value: '#EE5454'},
 });
