c0d085176490e906704a6d9948d2a9155811f0fb
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-17 21:09:21 +0100

    add styles

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,11 +1,16 @@
+import {Flex} from '@chakra-ui/react';
 import ModeToggle from './mode-toggle';
 
 const Header = () => {
     return (
-        <>
-            Header
+        <Flex
+            justify="space-between"
+            align="center"
+            py={{base: '1rem', md: '40px', lg: '0'}}
+            px={{base: '1.5rem', md: '64px', lg: '0'}}>
+            Quiz Title
             <ModeToggle />
-        </>
+        </Flex>
     );
 };
 
