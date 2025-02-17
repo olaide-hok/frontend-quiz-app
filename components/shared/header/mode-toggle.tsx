f544ba0d83334a7eec3a2c05289f4638023f0b7f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-17 21:10:19 +0100

    update component with styles and functionality

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -1,6 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
+import {useState} from 'react';
+
+import {Flex} from '@chakra-ui/react';
 import {Switch} from '@/components/ui/switch';
+import {LuMoon, LuSun} from 'react-icons/lu';
+import {useColorMode} from '@/components/ui/color-mode';
+
+// { checked: boolean | ((prevState: boolean) => boolean) }
 const ModeToggle = () => {
-    return <Switch>Toggle</Switch>;
+    const {toggleColorMode} = useColorMode();
+    const [checked, setChecked] = useState(false);
+
+    return (
+        <Flex gap="1rem">
+            <LuSun />
+            <Switch
+                checked={checked}
+                onCheckedChange={(e: any) => {
+                    setChecked(e.checked);
+                    toggleColorMode();
+                }}
+                colorPalette="brandPurple"
+                size="md"
+                css={{
+                    '& span[data-state=unchecked]': {bgColor: 'brandPurple'},
+                    '& span[data-state=unchecked] span[data-state=unchecked]': {
+                        bgColor: 'white',
+                    },
+                }}
+            />
+            <LuMoon />
+        </Flex>
+    );
 };
 
 export default ModeToggle;
