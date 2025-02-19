f6709bdf8214f7ecfe9000b6bb081f29df7af727
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-19 17:09:42 +0100

    add responsive styles

--- components/shared/subject-tile/index.tsx
+++ components/shared/subject-tile/index.tsx
@@ -5,23 +5,33 @@ import {Flex, Image} from '@chakra-ui/react';
 interface SubjectTileProps {
     name: string;
     src: string;
+    bgColor: string;
 }
-const SubjectTile = ({name, src}: SubjectTileProps) => {
+const SubjectTile = ({name, src, bgColor}: SubjectTileProps) => {
     return (
         <Flex
             as="button"
-            columnGap="1rem"
+            columnGap={{base: '1rem', lg: '2rem'}}
             align="center"
-            width="100%"
             onClick={() => console.log(name)}
             color="darkNavy"
             fontSize="18px"
             fontWeight="500"
-            p="12px"
+            p={{base: '12px', lg: '20px'}}
             borderRadius="12px"
             bgColor="white"
-            shadow="0px 16px 40px 0px rgba(143, 160, 193, 0.14)">
-            <Image src={src} alt={name} />
+            shadow="0px 16px 40px 0px rgba(143, 160, 193, 0.14)"
+            _hover={{cursor: 'pointer'}}
+            width={{base: '100%', lg: '564px'}}>
+            <Image
+                src={src}
+                alt={name}
+                bgColor={bgColor}
+                width={{base: '40px', lg: '56px'}}
+                height={{base: '40px', lg: '56px'}}
+                borderRadius="6px"
+                padding="5.71px"
+            />
             {name}
         </Flex>
     );
