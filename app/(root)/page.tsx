4a0448999181d1304e08260be21aa5b6a1a72319
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-19 17:10:44 +0100

    add responsive styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,30 +5,38 @@ const subjects = [
     {
         name: 'HTML',
         imgSrc: 'images/icon-html.svg',
+        bgColor: '#FFF1E9',
     },
     {
         name: 'CSS',
         imgSrc: 'images/icon-css.svg',
+        bgColor: '#E0FDEF',
     },
     {
         name: 'Javascript',
         imgSrc: 'images/icon-js.svg',
+        bgColor: '#EBF0FF',
     },
     {
         name: 'Accessibility',
         imgSrc: 'images/icon-accessibility.svg',
+        bgColor: '#F6E7FF',
     },
 ];
 export default function Home() {
     return (
         <Flex
             justify="space-between"
-            flexDir="column"
+            flexDir={{base: 'column', lg: 'row'}}
             py={{base: '2rem', md: '40px', lg: '0'}}
             px={{base: '1.5rem', md: '64px', lg: '0'}}
-            rowGap="40px">
+            rowGap={{base: '40px', md: '64px'}}>
             <Box>
-                <Box as="h1" textStyle="headingRegular" color="darkNavy">
+                <Box
+                    as="h1"
+                    textStyle="headingRegular"
+                    color="darkNavy"
+                    _dark={{color: 'white'}}>
                     Welcome to the
                 </Box>
                 <Box
@@ -36,17 +44,27 @@ export default function Home() {
                     textStyle="headingBold"
                     color="darkNavy"
                     mt="8px"
-                    mb="1rem">
+                    mb={{base: '1rem', lg: '48px'}}
+                    _dark={{color: 'white'}}>
                     Frontend Quiz!
                 </Box>
-                <Box as="p" textStyle="bodyItalic" color="greyNavy">
+                <Box
+                    as="p"
+                    textStyle="bodyItalic"
+                    color="greyNavy"
+                    _dark={{color: 'lightBluish'}}>
                     Pick a subject to get started.
                 </Box>
             </Box>
 
             <Flex flexDir="column" rowGap="12px">
-                {subjects.map(({name, imgSrc}) => (
-                    <SubjectTile key={name} name={name} src={imgSrc} />
+                {subjects.map(({name, imgSrc, bgColor}) => (
+                    <SubjectTile
+                        key={name}
+                        name={name}
+                        src={imgSrc}
+                        bgColor={bgColor}
+                    />
                 ))}
             </Flex>
         </Flex>
