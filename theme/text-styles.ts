16e4b20d5760a0adde858084b0d6a0b20804f18f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-20 21:22:20 +0100

    add extra textStyles definitions

--- theme/text-styles.ts
+++ theme/text-styles.ts
@@ -26,4 +26,28 @@ export const textStyles = defineTextStyles({
             lineHeight: '100%',
         },
     },
+    headingTitle: {
+        value: {
+            fontFamily: 'Rubik',
+            fontWeight: '500',
+            fontSize: {base: '18px', md: '28px'},
+            lineHeight: '100%',
+        },
+    },
+    bodyRegular: {
+        value: {
+            fontFamily: 'Rubik',
+            fontWeight: '500',
+            fontSize: {base: '20px', md: '36px'},
+            lineHeight: '120%',
+        },
+    },
+    bodySubtitle: {
+        value: {
+            fontFamily: 'Rubik',
+            fontWeight: '400',
+            fontSize: {base: '18px', md: '24px'},
+            lineHeight: '150%',
+        },
+    },
 });
