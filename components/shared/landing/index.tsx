15f408204d89e3d94e81b354dc3a88f4304e0ece
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-20 21:21:22 +0100

    import subjects array

--- components/shared/landing/index.tsx
+++ components/shared/landing/index.tsx
@@ -1,28 +1,6 @@
 import {Box, Flex} from '@chakra-ui/react';
 import SubjectTile from '@/components/shared/subject-tile';
-
-const subjects = [
-    {
-        name: 'HTML',
-        imgSrc: 'images/icon-html.svg',
-        bgColor: '#FFF1E9',
-    },
-    {
-        name: 'CSS',
-        imgSrc: 'images/icon-css.svg',
-        bgColor: '#E0FDEF',
-    },
-    {
-        name: 'Javascript',
-        imgSrc: 'images/icon-js.svg',
-        bgColor: '#EBF0FF',
-    },
-    {
-        name: 'Accessibility',
-        imgSrc: 'images/icon-accessibility.svg',
-        bgColor: '#F6E7FF',
-    },
-];
+import subjects from '@/subjects';
 
 const Landing = () => {
     return (
