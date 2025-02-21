89bc4e59ec6d547542273dcbebcef7d37dc61d6d
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-21 18:48:21 +0100

    create questionRecipe styles

---
+++ theme/question.recipe.ts
@@ -0,0 +1,30 @@
+import {defineSlotRecipe} from '@chakra-ui/react';
+
+export const questionRecipe = defineSlotRecipe({
+    slots: ['wrapper', 'questionWrapper', 'submitAndNextBtn'],
+    base: {
+        wrapper: {
+            display: 'flex',
+            alignItems: {base: 'center', lg: 'start'},
+            justifyContent: {lg: 'space-between'},
+            flexDir: {base: 'column', lg: 'row'},
+            rowGap: {base: '40px'},
+            px: {base: '1.5rem', md: '4rem', lg: 0},
+            pt: {base: '32px'},
+        },
+        questionWrapper: {
+            justifyContent: 'space-between',
+            gap: {base: '12px', md: '27px'},
+            width: {base: '100%', lg: '465px'},
+            height: {base: '100%', lg: '452px'},
+        },
+        submitAndNextBtn: {
+            w: '100%',
+            py: {base: '12px', md: '32px'},
+            bgColor: 'brandPurple',
+            color: 'white',
+            borderRadius: '12px',
+            shadow: '0px 16px 40px 0px rgba(143, 160, 193, 0.14)',
+        },
+    },
+});
