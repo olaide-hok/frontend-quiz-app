e228f3175c0bd47560edae21aa31c97472e46721
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-21 18:50:51 +0100

    refactor OptionTile and Question components.
    style both using individual recipes,.
    
    render questions dynamically.

--- components/shared/question/index.tsx
+++ components/shared/question/index.tsx
@@ -1,78 +1,80 @@
-import {Box, HStack, Image, Stack, VStack} from '@chakra-ui/react';
+import {useGlobalContext} from '@/context';
+import {
+    Box,
+    HStack,
+    Image,
+    Stack,
+    useSlotRecipe,
+    VStack,
+} from '@chakra-ui/react';
 import {Progress} from '@chakra-ui/react';
 import {useState} from 'react';
 
-const OptionTile = () => {
+interface OptionTileType {
+    text: string;
+    optLetter: string;
+}
+
+const OptionTile = ({text, optLetter}: OptionTileType) => {
     const [selected, setSelected] = useState(false);
+    const recipe = useSlotRecipe({key: 'optionTile'});
+    const optionTilestyles = recipe();
 
     return (
         <Box
             as="button"
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-            borderRadius="24px"
-            bgColor="white"
-            shadow="0px 16px 40px 0px rgba(143, 160, 193, 0.14)"
-            width={{base: '100%', lg: '564px'}}
-            p={{base: '12px', lg: '20px'}}
-            _dark={{bgColor: 'brandNavy'}}
-            _hover={{cursor: 'pointer'}}
-            _active={{borderColor: 'brandPurple', borderWidth: '3px'}}
-            _focus={{borderColor: 'brandPurple', borderWidth: '3px'}}
+            css={optionTilestyles.button}
             onClick={() => {
                 console.log('clicked me!');
-
                 setSelected(true);
             }}>
             <HStack gap={{base: '1rem', md: '32px'}}>
                 <Box
                     as="span"
                     textStyle="headingTitle"
-                    width={{base: '40px', md: '56px'}}
-                    height={{base: '40px', md: '56px'}}
                     bgColor={selected ? 'brandPurple' : 'lightGrey'}
                     color={selected ? 'white' : 'greyNavy'}
-                    py={{base: '11px', md: '14px'}}
-                    px={{base: '14px', md: '18px'}}
-                    borderRadius="12px">
-                    A
+                    css={optionTilestyles.option}>
+                    {optLetter}
                 </Box>
+
                 <Box as="span" textStyle="headingTitle">
-                    4.5 : 1
+                    {text}
                 </Box>
             </HStack>
-            <Image
-                src="images/icon-correct.svg"
-                alt="checkmark"
-                display="none"
-            />
-            <Image src="images/icon-incorrect.svg" alt="checkmark" display="" />
+            <HStack>
+                <Image
+                    src="images/icon-correct.svg"
+                    alt="checkmark"
+                    display="none"
+                />
+                <Image
+                    src="images/icon-incorrect.svg"
+                    alt="checkmark"
+                    display="none"
+                />
+            </HStack>
         </Box>
     );
 };
 
 const Question = () => {
+    const recipe = useSlotRecipe({key: 'question'});
+    const optionStyles = recipe();
+
+    const {questions, index, checkAnswer} = useGlobalContext();
+    const {questions: questionsArray} = questions[0];
+    const {answer, options, question} = questionsArray[index];
+
     return (
-        <Box
-            display="flex"
-            alignItems={{base: 'center', lg: 'start'}}
-            justifyContent={{lg: 'space-between'}}
-            flexDir={{base: 'column', lg: 'row'}}
-            rowGap={{base: '40px'}}
-            px={{base: '1.5rem', md: '4rem', lg: 0}}
-            pt={{base: '32px'}}>
-            <Stack
-                justify="space-between"
-                gap={{base: '12px', md: '27px'}}
-                width={{base: '100%', lg: '465px'}}
-                height={{base: '100%', lg: '452px'}}>
+        <Box css={optionStyles.wrapper}>
+            <Stack css={optionStyles.questionWrapper}>
                 <Box
                     as="p"
                     textStyle="bodyItalic"
                     color="greyNavy"
                     _dark={{color: 'lightBluish'}}>
-                    Question 6 of 10
+                    Question {index + 1} of {questionsArray.length}
                 </Box>
                 <Box
                     as="h1"
@@ -80,11 +82,12 @@ const Question = () => {
                     color="darkNavy"
                     _dark={{color: 'white'}}
                     flex="1">
-                    Which of these color contrast ratios defines the minimum
-                    WCAG 2.1 Level AA requirement for normal text?
+                    {question}
                 </Box>
 
                 <Progress.Root
+                    max={questionsArray.length}
+                    value={index + 1}
                     maxW="100%"
                     colorPalette="brandPurple"
                     mt={{base: '16px', md: '13px', lg: 0}}>
@@ -102,19 +105,24 @@ const Question = () => {
             <VStack
                 gap={{base: '12px', md: '24px'}}
                 width={{base: '100%', lg: '564px'}}>
-                <OptionTile />
-                <OptionTile />
-                <OptionTile />
-                <OptionTile />
+                {options?.map((opt: string, i: number) => {
+                    // Dynamically generate option letters (A, B, C, etc.)
+                    const optionLetter = String.fromCharCode(65 + i); // 65 is ASCII for 'A'
+                    return (
+                        <OptionTile
+                            key={opt}
+                            text={opt}
+                            optLetter={optionLetter}
+                        />
+                    );
+                })}
                 <Box
                     as="button"
                     textStyle="headingTitle"
-                    w="100%"
-                    py={{base: '12px', md: '32px'}}
-                    bgColor="brandPurple"
-                    color="white"
-                    borderRadius="12px"
-                    shadow="0px 16px 40px 0px rgba(143, 160, 193, 0.14)">
+                    css={optionStyles.submitAndNextBtn}
+                    onClick={() => {
+                        checkAnswer(answer);
+                    }}>
                     Submit Answer
                 </Box>
                 <HStack gap="8px" display="none">
