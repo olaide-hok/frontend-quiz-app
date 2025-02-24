b46578e346d5971211357558efb5e8c596de72e2
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-24 18:11:03 +0100

    create QuizTitle component

---
+++ components/shared/quiz-title/index.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import {Box, Image} from '@chakra-ui/react';
+
+interface QuizIconAndNameType {
+    name: string;
+    imgSrc: string;
+    bgColor: string;
+}
+
+interface QuizTitleType {
+    quiz: string;
+    subjects: QuizIconAndNameType[];
+}
+
+const QuizTitle = ({quiz, subjects}: QuizTitleType) => {
+    const {name, imgSrc, bgColor} =
+        subjects.find((sbj) => sbj.name === quiz) || {};
+
+    return (
+        <Box
+            display="flex"
+            alignItems="center"
+            columnGap={{base: '1rem', md: '24px'}}
+            textStyle="headingTitle"
+            color="darkNavy"
+            _dark={{color: 'white'}}>
+            <Image
+                src={imgSrc}
+                alt={name}
+                bgColor={bgColor}
+                width={{base: '40px', lg: '56px'}}
+                height={{base: '40px', lg: '56px'}}
+                borderRadius="6px"
+                padding="5.71px"
+            />
+            {name}
+        </Box>
+    );
+};
+
+export default QuizTitle;
