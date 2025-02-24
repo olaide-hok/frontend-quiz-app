68d6c4327412cf0fb6660b7fd1163d9719d363ec
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-24 18:10:23 +0100

    change quizIconAndName fn to QuizTitle component

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,41 +1,10 @@
 'use client';
 
-import {Box, Flex, Image} from '@chakra-ui/react';
+import {Flex} from '@chakra-ui/react';
 import ModeToggle from './mode-toggle';
 import {useGlobalContext} from '@/context';
 import subjects from '@/subjects';
-
-interface QuizIconAndNameType {
-    name: string;
-    imgSrc: string;
-    bgColor: string;
-}
-
-const quizIconAndName = (quiz: string, subjects: QuizIconAndNameType[]) => {
-    const {name, imgSrc, bgColor} =
-        subjects.find((sbj) => sbj.name === quiz) || {};
-
-    return (
-        <Box
-            display="flex"
-            alignItems="center"
-            columnGap={{base: '1rem', md: '24px'}}
-            textStyle="headingTitle"
-            color="darkNavy"
-            _dark={{color: 'white'}}>
-            <Image
-                src={imgSrc}
-                alt={name}
-                bgColor={bgColor}
-                width={{base: '40px', lg: '56px'}}
-                height={{base: '40px', lg: '56px'}}
-                borderRadius="6px"
-                padding="5.71px"
-            />
-            {name}
-        </Box>
-    );
-};
+import QuizTitle from '../quiz-title';
 
 const Header = () => {
     const {quiz} = useGlobalContext();
@@ -45,7 +14,7 @@ const Header = () => {
             align="center"
             py={{base: '1rem', md: '40px', lg: '0'}}
             px={{base: '1.5rem', md: '64px', lg: '0'}}>
-            {quiz ? quizIconAndName(quiz, subjects) : <div />}
+            {quiz ? <QuizTitle quiz={quiz} subjects={subjects} /> : <div />}
             <ModeToggle />
         </Flex>
     );
