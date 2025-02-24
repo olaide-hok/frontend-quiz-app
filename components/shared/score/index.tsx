32853689549bc859fa2a27b9b5e5713d4893267f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-24 18:15:26 +0100

    styled and conditionally render component based on showScore state value

--- components/shared/score/index.tsx
+++ components/shared/score/index.tsx
@@ -1,12 +1,19 @@
 import {useGlobalContext} from '@/context';
-import {Box, Stack, VStack} from '@chakra-ui/react';
+import {Box, Span, Stack, VStack} from '@chakra-ui/react';
+import QuizTitle from '../quiz-title';
+import subjects from '@/subjects';
 
 const Score = () => {
-    const {questions, quiz, correct} = useGlobalContext();
+    const {questions, quiz, correct, showScore} = useGlobalContext();
     const {questions: questionsArray} = questions[0];
 
     return (
-        <Stack direction={{base: 'column', lg: 'row'}}>
+        <Stack
+            display={showScore ? 'flex' : 'none'}
+            direction={{base: 'column', lg: 'row'}}
+            px={{base: '24px', md: '64px', lg: 0}}
+            pt={{base: '32px', md: '49px', lg: 0}}
+            justify="space-between">
             <Box>
                 <Box
                     as="h1"
@@ -22,18 +29,56 @@ const Score = () => {
                     mt="8px"
                     mb={{base: '1rem', lg: '48px'}}
                     _dark={{color: 'white'}}>
-                    You scored
+                    You scored...
                 </Box>
             </Box>
 
-            <VStack p={{base: '2rem', md: '3rem'}}>
-                <VStack gap={{base: '1rem', md: ''}}>
-                    <Box>{quiz}</Box>
-                    {correct}
-                    <Box>out of {questionsArray.length}</Box>
+            <VStack gap={{base: '12px', md: '2rem'}} w={{lg: '564px'}}>
+                <VStack
+                    p={{base: '2rem', md: '3rem'}}
+                    gap={{base: '1rem', md: ''}}
+                    borderRadius="12px"
+                    bgColor="white"
+                    width="100%"
+                    _dark={{
+                        bgColor: 'brandNavy',
+                    }}>
+                    {quiz ? <QuizTitle quiz={quiz} subjects={subjects} /> : ''}
+
+                    <Span
+                        fontSize={{base: '88px', md: '144px'}}
+                        lineHeight="100%"
+                        fontWeight="500"
+                        color="darkNavy"
+                        mt={{md: '24px'}}
+                        _dark={{
+                            color: 'white',
+                        }}>
+                        {correct}
+                    </Span>
+                    <Box
+                        as="span"
+                        textStyle="bodySubtitle"
+                        color="greyNavy"
+                        _dark={{
+                            color: 'lightBluish',
+                        }}>
+                        out of {questionsArray.length}
+                    </Box>
                 </VStack>
 
-                <Box as="button" textStyle="headingTitle">
+                <Box
+                    as="button"
+                    textStyle="headingTitle"
+                    bgColor="brandPurple"
+                    borderRadius="12px"
+                    py={{base: '12px', md: '2rem'}}
+                    color="white"
+                    width="100%"
+                    cursor="pointer"
+                    onClick={() => {
+                        console.log('Play Again!');
+                    }}>
                     Play Again
                 </Box>
             </VStack>
