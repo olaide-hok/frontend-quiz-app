7c64988d7f8e92a5cf7b905ff3ea2c0e697f74d1
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-03-03 16:11:26 +0100

    change 'Javascript' to 'JavaScript'

--- subjects.ts
+++ subjects.ts
@@ -1,24 +1,24 @@
 const subjects = [
-    {
-        name: 'HTML',
-        imgSrc: 'images/icon-html.svg',
-        bgColor: '#FFF1E9',
-    },
-    {
-        name: 'CSS',
-        imgSrc: 'images/icon-css.svg',
-        bgColor: '#E0FDEF',
-    },
-    {
-        name: 'Javascript',
-        imgSrc: 'images/icon-js.svg',
-        bgColor: '#EBF0FF',
-    },
-    {
-        name: 'Accessibility',
-        imgSrc: 'images/icon-accessibility.svg',
-        bgColor: '#F6E7FF',
-    },
+  {
+    name: "HTML",
+    imgSrc: "images/icon-html.svg",
+    bgColor: "#FFF1E9",
+  },
+  {
+    name: "CSS",
+    imgSrc: "images/icon-css.svg",
+    bgColor: "#E0FDEF",
+  },
+  {
+    name: "JavaScript",
+    imgSrc: "images/icon-js.svg",
+    bgColor: "#EBF0FF",
+  },
+  {
+    name: "Accessibility",
+    imgSrc: "images/icon-accessibility.svg",
+    bgColor: "#F6E7FF",
+  },
 ];
 
 export default subjects;
