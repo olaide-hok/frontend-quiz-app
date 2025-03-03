c132f0aea2845e395d441ffb37d39d3f89f29b0a
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-03-03 16:12:59 +0100

    added aria-labelledby prop

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -1,38 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-'use client';
-import {useState} from 'react';
+"use client";
+import { useState } from "react";
 
-import {Flex} from '@chakra-ui/react';
-import {Switch} from '@/components/ui/switch';
-import {LuMoon, LuSun} from 'react-icons/lu';
-import {useColorMode} from '@/components/ui/color-mode';
+import { Flex } from "@chakra-ui/react";
+import { Switch } from "@/components/ui/switch";
+import { LuMoon, LuSun } from "react-icons/lu";
+import { useColorMode } from "@/components/ui/color-mode";
 
 const ModeToggle = () => {
-    const {toggleColorMode} = useColorMode();
-    const [checked, setChecked] = useState(true);
+  const { toggleColorMode } = useColorMode();
+  const [checked, setChecked] = useState(true);
 
-    return (
-        <Flex gap="1rem" align="center">
-            <LuSun />
-            <Switch
-                checked={checked}
-                onCheckedChange={(e: any) => {
-                    setChecked(e.checked);
-                    toggleColorMode();
-                }}
-                colorPalette="brandPurple"
-                size="md"
-                css={{
-                    '& span[data-state=unchecked]': {bgColor: 'brandPurple'},
-                    '& span[data-state=unchecked] span[data-state=unchecked]': {
-                        bgColor: 'white',
-                    },
-                }}
-                label="toggle theme"
-            />
-            <LuMoon />
-        </Flex>
-    );
+  return (
+    <Flex gap="1rem" align="center">
+      <LuSun />
+      <Switch
+        checked={checked}
+        onCheckedChange={(e: any) => {
+          setChecked(e.checked);
+          toggleColorMode();
+        }}
+        colorPalette="brandPurple"
+        size="md"
+        css={{
+          "& span[data-state=unchecked]": { bgColor: "brandPurple" },
+          "& span[data-state=unchecked] span[data-state=unchecked]": {
+            bgColor: "white",
+          },
+        }}
+        label="toggle theme"
+        aria-labelledby="switch::R1akqut7::label"
+      />
+      <LuMoon />
+    </Flex>
+  );
 };
 
 export default ModeToggle;
