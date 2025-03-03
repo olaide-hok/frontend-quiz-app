c3b35276367d1091be85293bf4e325a56c341006
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-03-03 16:12:34 +0100

    removed role='heading' prop

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,25 +1,25 @@
-'use client';
+"use client";
 
-import {Flex} from '@chakra-ui/react';
-import ModeToggle from './mode-toggle';
-import {useGlobalContext} from '@/context';
-import subjects from '@/subjects';
-import QuizTitle from '../quiz-title';
+import { Flex } from "@chakra-ui/react";
+import ModeToggle from "./mode-toggle";
+import { useGlobalContext } from "@/context";
+import subjects from "@/subjects";
+import QuizTitle from "../quiz-title";
 
 const Header = () => {
-    const {quiz} = useGlobalContext();
-    return (
-        <Flex
-            as="header"
-            role="heading"
-            justify="space-between"
-            align="center"
-            py={{base: '1rem', md: '40px', lg: '0'}}
-            px={{base: '1.5rem', md: '64px', lg: '0'}}>
-            {quiz ? <QuizTitle quiz={quiz} subjects={subjects} /> : <div />}
-            <ModeToggle />
-        </Flex>
-    );
+  const { quiz } = useGlobalContext();
+  return (
+    <Flex
+      as="header"
+      justify="space-between"
+      align="center"
+      py={{ base: "1rem", md: "40px", lg: "0" }}
+      px={{ base: "1.5rem", md: "64px", lg: "0" }}
+    >
+      {quiz ? <QuizTitle quiz={quiz} subjects={subjects} /> : <div />}
+      <ModeToggle />
+    </Flex>
+  );
 };
 
 export default Header;
